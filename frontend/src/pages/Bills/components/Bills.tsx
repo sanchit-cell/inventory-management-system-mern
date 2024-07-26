5e45c7d95cee4272761a634ea1c8b9209acbc5c2
Sanchit Pandey <sanchitpandey08@gmail.com>
2024-07-26 16:21:17 +0530

    first commit

---
+++ frontend/src/pages/Bills/components/Bills.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdvancedFilterDemo from './Table'
+
+const Bills = () => {
+  return (
+    <div>
+        
+        <AdvancedFilterDemo/>
+      
+    </div>
+  )
+}
+
+export default Bills
